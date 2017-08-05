4ff897e42eed79ef27a101b5f32646fb23aa26ca
Muhriddin <ismoilovmuh1996@gmail.com>
2017-08-05 17:04:26 +0900

    gradle update

--- app/src/main/java/uz/ismoilov/muhriddin/neomemo/LoginActivity.java
+++ app/src/main/java/uz/ismoilov/muhriddin/neomemo/LoginActivity.java
@@ -1,9 +1,9 @@
 package uz.ismoilov.muhriddin.neomemo;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -12,13 +12,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.io.Serializable;
 
