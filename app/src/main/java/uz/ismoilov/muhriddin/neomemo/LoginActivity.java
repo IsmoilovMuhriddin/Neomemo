fad427e48cae81643bfec64a1ba7f58f8c80bf41
Muhriddin <ismoilovmuh1996@gmail.com>
2017-08-05 17:06:34 +0900

    gradle update auth

--- app/src/main/java/uz/ismoilov/muhriddin/neomemo/LoginActivity.java
+++ app/src/main/java/uz/ismoilov/muhriddin/neomemo/LoginActivity.java
@@ -12,6 +12,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.io.Serializable;
 
